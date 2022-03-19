65cdc23ec99f59e24c55684e61069e848f13498c
Seth Hendrick <seth@shendrick.net>
2022-03-19 17:32:17 -0400

    Started adding tests.

---
+++ _siteplugin/SiteTests/PostMetadataTests.cs
@@ -0,0 +1,74 @@
+﻿//
+// SiteTests - Site Tests for meditationenthusiasts.org
+// Copyright (C) 2022 Meditation Enthusiasts.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using NUnit.Framework;
+using Pretzel.Logic.Extensions;
+
+namespace SiteTests
+{
+    [TestFixture]
+    public sealed class PostMetadataTests
+    {
+        // ---------------- Tests -----------------
+
+        [Test]
+        public void WpidIsUniqueTest()
+        {
+            // Setup
+            var foundIds = new Dictionary<int, string>();
+
+            // Act / Check
+            PerformActionOnPost(
+                ( postPath, metadata ) =>
+                {
+                    if( metadata.ContainsKey( "wpid" ) == false )
+                    {
+                        return;
+                    }
+
+                    bool parsed = int.TryParse( metadata["wpid"].ToString(), out int wpid );
+                    Assert.IsTrue( parsed, $"{postPath} contains a non-int wpid" );
+
+                    if( foundIds.ContainsKey( wpid ) )
+                    {
+                        Assert.Fail( $"{postPath} ID is a duplicate with {foundIds[wpid]}!" );
+                    }
+
+                    foundIds.Add( wpid, postPath );
+                }
+            );
+        }
+
+        // ---------------- Test Helpers -----------------
+
+        private void PerformActionOnPost( Action<string, IDictionary<string, object>> postAction )
+        {
+            foreach( string postPath in Directory.GetFiles( TestContants.PostsDirectory ) )
+            {
+                string fileContent = File.ReadAllText( postPath );
+
+                IDictionary<string, object> metadata = fileContent.YamlHeader();
+                postAction( postPath, metadata );
+            }
+        }
+    }
+}
---
+++ _siteplugin/SiteTests/TestContants.cs
@@ -0,0 +1,61 @@
+﻿//
+// SiteTests - Site Tests for meditationenthusiasts.org
+// Copyright (C) 2022 Meditation Enthusiasts.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace SiteTests
+{
+    internal static class TestContants
+    {
+        // ---------------- Fields ----------------
+
+        public static readonly string RepoRoot;
+
+        public static readonly string PostsDirectory;
+
+        public static readonly string SiteOutput;
+
+        public static readonly ushort Port;
+
+        // ---------------- Constructor ----------------
+
+        static TestContants()
+        {
+            string assemblyDir = TestContext.CurrentContext.TestDirectory;
+            RepoRoot = Path.Combine(
+                assemblyDir,
+                "..",
+                "..",
+                "..",
+                "..",
+                ".."
+            );
+
+            PostsDirectory = Path.Combine( RepoRoot, "_posts" );
+            SiteOutput = Path.Combine( RepoRoot, "_site" );
+
+            string testPortStr = Environment.GetEnvironmentVariable( "TEST_PORT" ) ?? string.Empty;
+            if( ushort.TryParse( testPortStr, out Port ) == false )
+            {
+                Port = 8080;
+            }
+        }
+    }
+}
